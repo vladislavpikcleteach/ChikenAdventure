3bf9c4d2e44c6ed71281a7ec481e1b530a74ae04
Vlad <vlad@macbookpro-1.home>
2025-09-13 17:33:37 +0200

    First commit

---
+++ ChickenAdventure/ChickenAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenAdventureApp.swift
+//  ChickenAdventure
+//
+//  Created by Vlad on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChickenAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickenAdventure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChickenAdventure
+//
+//  Created by Vlad on 13/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
